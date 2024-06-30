43e938a53c73cc7ece2b74eed12e3193aacd9625
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2024-06-30 17:53:00 +0200

    try things in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='seg_hub',
+    name='seghub',
     version='0.0.1',
-    description='Multiple tools for segmentation with focus on combining variable feature extractors with a random forest classifier.',
+    description='Tools for segmentation, with focus on combining variable feature extractors with a random forest classifier.',
     author='Roman Schwob',
     author_email='roman.schwob@students.unibe.ch',
     license='GNU GPLv3',
-    packages=['.'],
+    packages=['./src'],
     zip_safe=False
     )
