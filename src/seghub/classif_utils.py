af40b0e4926b1c8f79e02d67f734f70a70226398
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2024-07-15 09:55:34 +0200

    add features_combinator function

--- src/seghub/classif_utils.py
+++ src/seghub/classif_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from skimage import filters, morphology
 
 def get_pca_features(features, num_components=2):
     '''
@@ -42,3 +43,36 @@ def get_kmeans_clusters(features, num_clusters=3):
     if len(features.shape) > 2:
         cluster_labels = cluster_labels.reshape(features.shape[0], features.shape[1])
     return cluster_labels
+
+def features_combinator(image, features_func_list, features_cfg_list, num_pcs_list=None, feature_smoothness_list=None, img_as_feature=False):
+    '''
+    Takes an image and a list of feature functions and their configurations, and returns a combined feature space.
+    INPUT:
+        image (np.ndarray): input image, shape (H, W, C)
+        features_func_list (list): list of feature functions to apply
+            each function must take an image of shape (H, W, C) or (H, W)
+            and return features of shape (H, W, n_features) (feature space)
+        features_cfg_list (list): list of configurations for each feature function
+        num_pcs_list (list): list of number of principal components to extract for each feature
+        feature_smoothness_list (list): list of smoothness parameters for each feature
+    OUTPUT:
+        features_combined (np.ndarray): combined feature space, shape (H, W, F)
+    '''
+    features_list = []
+    for i, features_func in enumerate(features_func_list):
+        features = features_func(image, **features_cfg_list[i])
+        # Get principal components if desired
+        if num_pcs_list is not None and num_pcs_list[i] > 0:
+            features = get_pca_features(features, num_components=num_pcs_list[i])
+        # Smoothen features if desired
+        if feature_smoothness_list is not None and feature_smoothness_list[i] > 0:
+            feature_space = np.moveaxis(feature_space, 2, 0)
+            feature_space = np.array([filters.median(f, footprint=morphology.disk(feature_smoothness_list[i])) for f in feature_space])
+            feature_space = np.moveaxis(feature_space, 0, 2)
+            features_list.append(features)
+        # Add image pixel values (of each channel) as features
+        if len(image.shape) == 2:
+            image = np.expand_dims(image, axis=2)
+        feature_space = np.dstack((feature_space, image))
+    features_combined = np.stack(features_list, axis=-1)
+    return features_combined
